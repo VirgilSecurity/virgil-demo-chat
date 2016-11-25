9446f6fb7aa828b06e2310275345316d9d578fec
Vadim Avdeev <v.avdeiev@gmail.com>
2016-11-25 16:03:21 +0200

    fix: correct name of env variable

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.port || 8090,
+  port: process.env.PORT || 8090,
 	virgilCardId: process.env.VIRGIL_APP_CARD_ID,
 	privateKey: process.env.VIRGIL_APP_PRIVATE_KEY,
 	privateKeyPassword: process.env.VIRGIL_APP_PRIVATE_KEY_PWD
